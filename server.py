e949255683d95908ab2b4acb6ce2851715475419
bi3mer <colan@comcast.net>
2019-10-21 16:42:07 -0400

    removed useless code

--- server.py
+++ server.py
@@ -70,13 +70,3 @@ def run():
 
 if __name__ == "__main__":
     run()
-
-if __name__ == '__main__':
-    log.info('logging info is working')
-
-    log.warning('logging warning is working')
-    log.error('logging error is working')
-
-    model = Model()
-    toxic_sentence = 'down with colan!'
-    log.info(f'Random Score: {model.score(toxic_sentence)}')
