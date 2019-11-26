7de65fc705e6b01fdd8310417e6a6652d6fcee5c
bi3mer <colan@comcast.net>
2019-11-26 16:54:40 -0500

    bert pre processing for new data

--- Toxicity/bert_pre_process_data.py
+++ Toxicity/bert_pre_process_data.py
@@ -1,4 +1,3 @@
-from sklearn.model_selection import train_test_split
 from pytorch_pretrained_bert import BertTokenizer
 from collections import Counter
 from tqdm import tqdm
@@ -11,50 +10,54 @@ import os
 from Utility import log
 import config
 
-def process_data(model_type, size=None):
-    if size == None:
-        training_data_path = os.path.join('.', 'data', f'{model_type}_training_data.pkl')
-        testing_data_path = os.path.join('.', 'data', f'{model_type}_testing_data.pkl')
-    else:
-        training_data_path = os.path.join('.', 'data', f'{model_type}_{size}_training_data.pkl')
-        testing_data_path = os.path.join('.', 'data', f'{model_type}_{size}_testing_data.pkl')
-
-    split_percentage = 0.8
-
-    log.info('Loading Training Data')
-    if size == None:
-        df = pd.read_csv(config.TRAIN_PATH, header=0, usecols=['target', 'comment_text'])
-    else:
-        df = pd.read_csv(config.TRAIN_PATH, header=0, nrows=size, usecols=['target', 'comment_text'])
-
-    if 'uncased' in model_type:
-        log.info('Converting text to lower case')
-        targets = df['comment_text'].apply(lambda comment: comment.lower())
-        df.update(targets)
-
-    log.info('Tokenizing text and formatting training data')
-    tokenizer = BertTokenizer.from_pretrained(model_type)
+def tokenize_df(df, tokenizer):
     data = df.to_dict()
-    data_set = []
+    x = []
+    y = []
 
     for key in tqdm(data['target'], ascii=True):
         # max input is 512 for BERT network and we have to one at the start and one at the end
         tokenized_text = tokenizer.tokenize(f'[CLS] {data["comment_text"][key]}')[:139]
         tokenized_text.append('[CLS]')
         tokenized_ids = tokenizer.convert_tokens_to_ids(tokenized_text)
-        mid_point = len(tokenized_ids) / 2
 
-        while len(tokenized_ids) < 140:
-            tokenized_ids.append(0)        
+        x.append(tokenized_ids)
+        y.append(data['target'][key])
 
-        segment_ids = [0 if i < mid_point else 1 for i in range(len(tokenized_ids))]
-        data_set.append((tokenized_ids, segment_ids, data['target'][key]))
+    return [x, y]
 
-    log.info(f'Splitting data with {split_percentage * 100}% as part of the training data')
-    training_set, testing_set = train_test_split(data_set, test_size = 1.0 - split_percentage)
+def process_data(model_type, size=None):
+    training_data_path = os.path.join('.', 'data', f'{model_type}_{size}_training_data.pkl')
+    testing_data_path = os.path.join('.', 'data', f'{model_type}_{size}_testing_data.pkl')
 
-    log.info(f'Train Data Size: {len(training_set)}')
-    log.info(f'Test Data Size: {len(testing_set)}')
+    log.info('Loading Training Data')
+    path = os.path.join(config.BASE_DIR, 'sampled_training.csv')
+    train_df = pd.read_csv(path, header=0, nrows=size, usecols=['target', 'comment_text'])
+
+    log.info('loading testing data')
+    path = os.path.join(config.BASE_DIR, 'unsampled_testing.csv')
+    test_df = pd.read_csv(path, header=0, nrows=size, usecols=['target', 'comment_text'])
+
+    if 'uncased' in model_type:
+        log.info('Converting training text to lower case')
+        targets = train_df['comment_text'].apply(lambda comment: comment.lower())
+        train_df.update(targets)
+
+        log.info('Converting testing text to lower case')
+        targets = test_df['comment_text'].apply(lambda comment: comment.lower())
+        test_df.update(targets)
+
+    log.info('loading tokenizer')
+    tokenizer = BertTokenizer.from_pretrained(model_type)
+
+    log.info('tokenizing training data')
+    training_set = tokenize_df(train_df, tokenizer)
+
+    log.info('tokenizing testing data')
+    testing_set = tokenize_df(test_df, tokenizer)
+
+    log.info(f'Train Data Size: {len(train_df)}')
+    log.info(f'Test Data Size: {len(test_df)}')
 
     pickle.dump(training_set, open(training_data_path, 'wb'))
     log.info(f'Saved training data to {training_data_path}')
